906e380c7311d764eb72509d52282081153c9d5c
Mike Williams <mumakin@gmail.com>
2021-01-03 13:22:54 -0700

    game over works

--- simplest/gravitygame.py
+++ simplest/gravitygame.py
@@ -9,6 +9,7 @@ SCREEN_TITLE = "Gravity Game"
 OCEAN_WIDTH = 427
 LAND_WIDTH = 426
 EARTH_HEIGHT = 100
+GAME_OVER_FONT_SIZE = 50
 
 SHIP_FILE_WIDTH = 840
 SHIP_FILE_HEIGTH = 1510
@@ -16,8 +17,10 @@ SHIP_SCALE = 0.1
 SHIP_OFF_PATH = "../assets/rocket_off.png"
 SHIP_ON_PATH = "../assets/rocket_on.png"
 
-ACCELERATION_GRAVITY = -50
-ACCELERATION_ROCKET = 30
+ACCELERATION_GRAVITY = -100
+ACCELERATION_ROCKET = 50
+
+SHIP_CRASH_VELOCITY = -200
 
 class MyGame(arcade.Window):
     def __init__(self, width, height):
@@ -34,6 +37,8 @@ class MyGame(arcade.Window):
 
         #game state
         self.paused = False
+        self.game_over = False
+        self.game_over_message = ""
 
         # create the ship off sprite
         self.ship_off = arcade.Sprite(SHIP_OFF_PATH, SHIP_SCALE)
@@ -47,6 +52,7 @@ class MyGame(arcade.Window):
         self.ship_on.bottom = self.ship_y
         self.ship_on.alpha = 0
 
+        # keep the ship sprites in a sprite list which is faster later
         self.ship_list = arcade.SpriteList()
         self.ship_list.append(self.ship_on)
         self.ship_list.append(self.ship_off)
@@ -54,6 +60,9 @@ class MyGame(arcade.Window):
 
     def on_draw(self):
         arcade.start_render()
+
+        if self.game_over:
+            arcade.draw_text(self.game_over_message, SCREEN_WIDTH/2, SCREEN_HEIGHT/2, arcade.color.BLACK, GAME_OVER_FONT_SIZE, align="center", anchor_x="center", anchor_y="center")
 
         # draw the ground as two blue rectangles with a green in the  middle
         earth_x_cursor = 0
@@ -88,16 +97,23 @@ class MyGame(arcade.Window):
 
 
     def on_update(self, delta_time: float):
+        if self.game_over:
+            return
+
         if self.paused:
             return
 
         # check for on the ground
         if EARTH_HEIGHT >= self.ship_y:
+            if self.ship_y_velocity < SHIP_CRASH_VELOCITY:
+                self.game_over_message = "GAME OVER\nYou crashed at velocity\n" + str(round(self.ship_y_velocity,1))
+                self.game_over = True
+
             self.ship_y_velocity = 0
             self.ship_y = EARTH_HEIGHT
             if not self.ship_engine_on:
                 return
-                
+
         #move the ship
         self.ship_y_velocity += self.ship_acceleration * delta_time
         self.ship_y += self.ship_y_velocity * delta_time
