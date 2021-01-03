05f1dcad94a747e1e0df7a3cfd7e601d21b97905
Mike Williams <mumakin@gmail.com>
2021-01-03 12:24:52 -0700

    basic mechanic in place

--- simplest/gravitygame.py
+++ simplest/gravitygame.py
@@ -15,13 +15,22 @@ SHIP_FILE_HEIGTH = 1510
 SHIP_SCALE = 0.1
 SHIP_PATH = "../assets/rocket_off.png"
 
+ACCELERATION_GRAVITY = -5
+ACCELERATION_ROCKET = 3
+
 class MyGame(arcade.Window):
     def __init__(self, width, height):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
         arcade.set_background_color(arcade.color.WHITE)
 
+        self.paused = False
+        self.ship_acceleration = ACCELERATION_GRAVITY
+
     def setup(self):
-        pass
+        # create the ship
+        self.ship = arcade.Sprite(SHIP_PATH, SHIP_SCALE)
+        self.ship.center_x = SCREEN_WIDTH / 2
+        self.ship.bottom = SCREEN_HEIGHT - SHIP_FILE_HEIGTH * SHIP_SCALE
 
     def on_draw(self):
         arcade.start_render()
@@ -34,15 +43,22 @@ class MyGame(arcade.Window):
         earth_x_cursor += LAND_WIDTH
         arcade.draw_xywh_rectangle_filled(earth_x_cursor, 0, OCEAN_WIDTH, EARTH_HEIGHT, arcade.color.BLUE)
 
-        ship_list = arcade.SpriteList()
-        ship = arcade.Sprite(SHIP_PATH, SHIP_SCALE)
-        ship.center_x = SCREEN_WIDTH / 2
-        ship.bottom = SCREEN_HEIGHT - SHIP_FILE_HEIGTH * SHIP_SCALE
-        ship_list.append(ship)
-        ship_list.draw()
+        self.ship.draw()
 
-        ship.change_y = -100
+    def on_key_press(self, symbol, modifiers):
+        if arcade.key.W == symbol:
+            self.ship_acceleration = ACCELERATION_ROCKET
 
+    def on_update(self, delta_time: float):
+        if self.paused:
+            return
+        
+        #move the ship
+        self.ship.change_y += self.ship_acceleration * delta_time
+        self.ship.update()
+
+
+    
 def main():
     game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
     game.setup()
