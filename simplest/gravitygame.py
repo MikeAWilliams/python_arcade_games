b1133878f0ace046b6f8b9ca273054c4dfb3500a
Mike Williams <mumakin@gmail.com>
2021-01-03 12:02:53 -0700

    go oo

--- simplest/gravitygame.py
+++ simplest/gravitygame.py
@@ -13,28 +13,41 @@ EARTH_HEIGHT = 100
 SHIP_FILE_WIDTH = 840
 SHIP_FILE_HEIGTH = 1510
 SHIP_SCALE = 0.1
-SHIP_PATH = "../assets/rocket_off.jpg"
+SHIP_PATH = "../assets/rocket_off.png"
 
-# Setup the drawing environment
-arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-arcade.set_background_color(arcade.color.WHITE)
-arcade.start_render()
+class MyGame(arcade.Window):
+    def __init__(self, width, height):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        arcade.set_background_color(arcade.color.WHITE)
 
-# draw the ground as two blue rectangles with a green in the  middle
-earth_x_cursor = 0
-arcade.draw_xywh_rectangle_filled(earth_x_cursor, 0, OCEAN_WIDTH, EARTH_HEIGHT, arcade.color.BLUE)
-earth_x_cursor += OCEAN_WIDTH
-arcade.draw_xywh_rectangle_filled(earth_x_cursor, 0, LAND_WIDTH, EARTH_HEIGHT, arcade.color.GREEN)
-earth_x_cursor += LAND_WIDTH
-arcade.draw_xywh_rectangle_filled(earth_x_cursor, 0, OCEAN_WIDTH, EARTH_HEIGHT, arcade.color.BLUE)
+    def setup(self):
+        pass
 
-ship = arcade.Sprite(SHIP_PATH, SHIP_SCALE)
-ship.center_x = SCREEN_WIDTH / 2
-ship.bottom = SCREEN_HEIGHT - SHIP_FILE_HEIGTH * SHIP_SCALE
-ship.draw()
+    def on_draw(self):
+        arcade.start_render()
 
-# done drawing
-arcade.finish_render()
+        # draw the ground as two blue rectangles with a green in the  middle
+        earth_x_cursor = 0
+        arcade.draw_xywh_rectangle_filled(earth_x_cursor, 0, OCEAN_WIDTH, EARTH_HEIGHT, arcade.color.BLUE)
+        earth_x_cursor += OCEAN_WIDTH
+        arcade.draw_xywh_rectangle_filled(earth_x_cursor, 0, LAND_WIDTH, EARTH_HEIGHT, arcade.color.GREEN)
+        earth_x_cursor += LAND_WIDTH
+        arcade.draw_xywh_rectangle_filled(earth_x_cursor, 0, OCEAN_WIDTH, EARTH_HEIGHT, arcade.color.BLUE)
 
-# start the game
-arcade.run()
+        ship_list = arcade.SpriteList()
+        ship = arcade.Sprite(SHIP_PATH, SHIP_SCALE)
+        ship.center_x = SCREEN_WIDTH / 2
+        ship.bottom = SCREEN_HEIGHT - SHIP_FILE_HEIGTH * SHIP_SCALE
+        ship_list.append(ship)
+        ship_list.draw()
+
+        ship.change_y = -100
+
+def main():
+    game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
+    game.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
