a502778c072039bfcccedec3891ed2fe938e8baf
Mike Williams <mumakin@gmail.com>
2021-01-30 19:46:07 -0700

    player moves

--- wompus/game.py
+++ wompus/game.py
@@ -19,11 +19,16 @@ class WompusGame(arcade.Window):
         arcade.set_background_color(arcade.color.WHITE)
         self.grid = grid.Grid(width, height, GRID_COLUMNS, GRID_ROWS)
         self.player = player.Player(self.grid.column_width, self.grid.row_height)
+        self.playerI = 0
+        self.playerJ = 0
 
     def setup(self):
         #game state
         self.game_over = False
         self.game_over_message = ""
+        self.playerI = random.randint(0, GRID_COLUMNS - 1)
+        self.playerJ = random.randint(0, GRID_ROWS - 1)
+        self.player.SetIJ(self.playerI, self.playerJ)
 
 
     def on_draw(self):
@@ -47,7 +52,44 @@ class WompusGame(arcade.Window):
             return
 
     def on_key_release(self, symbol, modifiers):
-        pass
+        if self.game_over:
+            return
+        if arcade.key.A == symbol:
+            self.TryToMoveLeft()
+        elif arcade.key.W == symbol:
+            self.TryToMoveUp()
+        elif arcade.key.D == symbol:
+            self.TryToMoveRight()
+        elif arcade.key.S == symbol:
+            self.TryToMoveDown()
+    
+    def TryToMoveDown(self):
+        if self.playerJ <= 0:
+            return
+        self.playerJ-=1
+        self.player.SetIJ(self.playerI, self.playerJ)
+
+    def TryToMoveUp(self):
+        if self.playerJ >= GRID_ROWS - 1:
+            return
+        self.playerJ+=1
+        self.player.SetIJ(self.playerI, self.playerJ)
+
+    def TryToMoveRight(self):
+        if self.playerI >= GRID_COLUMNS - 1:
+            return
+        self.playerI+=1
+        self.player.SetIJ(self.playerI, self.playerJ)
+
+    def TryToMoveLeft(self):
+        if self.playerI <= 0:
+            return
+        self.playerI-=1
+        self.player.SetIJ(self.playerI, self.playerJ)
+
+    def on_update(self, delta_time: float):
+        if self.game_over:
+            return
 
     def on_update(self, delta_time: float):
         if self.game_over:
--- wompus/player.py
+++ wompus/player.py
@@ -10,5 +10,9 @@ class Player():
         self.sprite.center_x = width / 2
         self.sprite.center_y = height / 2
 
+    def SetIJ(self, i, j):
+        self.sprite.center_x = i * self.sprite.width + self.sprite.width / 2
+        self.sprite.center_y = j * self.sprite.height + self.sprite.height / 2
+
     def Draw(self):
         self.sprite.draw()
