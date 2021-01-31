0cbc838129f8e3987e742d7a3fb5510625df6349
Mike Williams <mumakin@gmail.com>
2021-01-30 20:00:38 -0700

    all we need is a warning

--- wompus/game.py
+++ wompus/game.py
@@ -13,22 +13,42 @@ GAME_OVER_FONT_SIZE = 50
 GRID_COLUMNS = 10
 GRID_ROWS = 5
 
+class Position():
+    def __init__(self, i, j):
+        self.I = i
+        self.J = j
+    def Intersecrts(self, other):
+        return self.I == other.I and self.J == other.J
+
+def GetRandomUnocupiedPosition(ocupied):
+    while True:
+        position = Position(random.randint(0, GRID_COLUMNS - 1), random.randint(0, GRID_ROWS - 1))
+        good = True
+        for other in ocupied:
+            if position.Intersecrts(other):
+                good = False
+                break
+        if good:
+            break
+    return position
+
+
 class WompusGame(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height, SCREEN_TITLE)
         arcade.set_background_color(arcade.color.WHITE)
         self.grid = grid.Grid(width, height, GRID_COLUMNS, GRID_ROWS)
         self.player = player.Player(self.grid.column_width, self.grid.row_height)
-        self.playerI = 0
-        self.playerJ = 0
+        self.playerPosition = Position(0, 0)
 
     def setup(self):
         #game state
         self.game_over = False
         self.game_over_message = ""
-        self.playerI = random.randint(0, GRID_COLUMNS - 1)
-        self.playerJ = random.randint(0, GRID_ROWS - 1)
-        self.player.SetIJ(self.playerI, self.playerJ)
+        self.playerPosition = GetRandomUnocupiedPosition([])
+        self.player.SetIJ(self.playerPosition.I, self.playerPosition.J)
+        self.wompusPosition = GetRandomUnocupiedPosition([self.playerPosition])
+        self.exitPosition = GetRandomUnocupiedPosition([self.playerPosition, self.wompusPosition])
 
 
     def on_draw(self):
@@ -62,30 +82,40 @@ class WompusGame(arcade.Window):
             self.TryToMoveRight()
         elif arcade.key.S == symbol:
             self.TryToMoveDown()
+        self.CheckEndGame()
     
+    def CheckEndGame(self):
+        if self.playerPosition.Intersecrts(self.wompusPosition):
+            self.game_over = True
+            self.game_over_message = "You were killed by the wompus"
+
+        if self.playerPosition.Intersecrts(self.exitPosition):
+            self.game_over = True
+            self.game_over_message = "You escaped"
+
     def TryToMoveDown(self):
-        if self.playerJ <= 0:
+        if self.playerPosition.J <= 0:
             return
-        self.playerJ-=1
-        self.player.SetIJ(self.playerI, self.playerJ)
+        self.playerPosition.J-=1
+        self.player.SetIJ(self.playerPosition.I, self.playerPosition.J)
 
     def TryToMoveUp(self):
-        if self.playerJ >= GRID_ROWS - 1:
+        if self.playerPosition.J >= GRID_ROWS - 1:
             return
-        self.playerJ+=1
-        self.player.SetIJ(self.playerI, self.playerJ)
+        self.playerPosition.J+=1
+        self.player.SetIJ(self.playerPosition.I, self.playerPosition.J)
 
     def TryToMoveRight(self):
-        if self.playerI >= GRID_COLUMNS - 1:
+        if self.playerPosition.I >= GRID_COLUMNS - 1:
             return
-        self.playerI+=1
-        self.player.SetIJ(self.playerI, self.playerJ)
+        self.playerPosition.I+=1
+        self.player.SetIJ(self.playerPosition.I, self.playerPosition.J)
 
     def TryToMoveLeft(self):
-        if self.playerI <= 0:
+        if self.playerPosition.I <= 0:
             return
-        self.playerI-=1
-        self.player.SetIJ(self.playerI, self.playerJ)
+        self.playerPosition.I-=1
+        self.player.SetIJ(self.playerPosition.I, self.playerPosition.J)
 
     def on_update(self, delta_time: float):
         if self.game_over:
