5b1c8b5a0b60f280e890a5fa526b97c3765a69d5
Mike Williams <mumakin@gmail.com>
2024-06-30 19:36:06 -0600

    lots of comments

--- barcode/barcodeWriter.py
+++ barcode/barcodeWriter.py
@@ -2,6 +2,8 @@ import png
 import sys
 
 
+# given data which is a list of lists of 0 and one write that to a png file with file_name
+# example input would be [[0, 1, 0, 0, 1, 0, 0, 0], [0, 1, 1, 0, 1, 0, 0, 1]]
 def write_png(data, file_name):
     writer = png.Writer(len(data[0]), len(data), greyscale=True, bitdepth=1)
     file = open(file_name, "wb")
@@ -9,6 +11,8 @@ def write_png(data, file_name):
     file.close()
 
 
+# add 0 in front of the string until it has 8 digits
+# so 111 is converted into 0000111
 def ensure_eight_bits(binary_letter):
     needed_zeros = 8 - len(binary_letter)
     for i in range(needed_zeros):
@@ -16,6 +20,8 @@ def ensure_eight_bits(binary_letter):
     return binary_letter
 
 
+# given a string of 0 and 1 like 0000111 convert that to a list of integer 0 and 1
+# so 0000111 becomes [0,0,0,0,1,1,1]
 def binary_string_to_int_list(binary_string):
     result = []
     for binary_char in binary_string:
@@ -23,21 +29,37 @@ def binary_string_to_int_list(binary_string):
     return result
 
 
+# given a secret message convert it to a list of integer lists
+# like Hi = [[0, 1, 0, 0, 1, 0, 0, 0], [0, 1, 1, 0, 1, 0, 0, 1]]
 def string_to_binary_array(string_message):
     result = []
     for letter in string_message:
-        ascii_letter = ord(letter)
-        binary_string_letter = "{0:b}".format(ascii_letter)
+        # ord converts a letter to an ascii integer see https://www.asciitable.com/
+        ascii_int_letter = ord(letter)
+        # now convert the ascii integer into a binary string
+        binary_string_letter = "{0:b}".format(ascii_int_letter)
+        # add 0 to the front so that we always have 8 digits 0 or 1
         binary_string_letter = ensure_eight_bits(binary_string_letter)
+        # convert the string to a list of integers
+        # so H = [0, 1, 0, 0, 1, 0, 0, 0]
         binary_int_list = binary_string_to_int_list(binary_string_letter)
         result.append(binary_int_list)
     return result
 
 
+# the program starts here, above this is functions
+
+# check to make sure they provided a secret message. Tell them how to call this if they didn't
 if len(sys.argv) != 2:
     print('Usage python barcodeWriter.py "the secret message"')
     exit()
-message = sys.argv[1]
-binary = string_to_binary_array(message)
 
-write_png(binary, "code.png")
+# get the message from the command line argument
+message = sys.argv[1]
+
+# convert the message to binary example result would be [[0, 1, 0, 0, 1, 0, 0, 0], [0, 1, 1, 0, 1, 0, 0, 1]]
+binaryDigits = string_to_binary_array(message)
+
+# write the binary to a png file
+# example input would be [[0, 1, 0, 0, 1, 0, 0, 0], [0, 1, 1, 0, 1, 0, 0, 1]]
+write_png(binaryDigits, "code.png")
