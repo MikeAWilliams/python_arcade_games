d1b0a93a2bd1078e884d721d56b3cc12bf459e17
Mike Williams <mumakin@gmail.com>
2026-05-17 15:44:13 -0600

    no working

--- rogelike/maze_viewer.py
+++ rogelike/maze_viewer.py
@@ -1,13 +1,18 @@
 import arcade
+import random
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Game"
-DISPLAY_SCALE = 4
+SCREEN_TITLE = "Maze Viewer"
+DISPLAY_SCALE = 1
+RAW_TILE_SIZE = 12
+TILE_SIZE = RAW_TILE_SIZE * DISPLAY_SCALE
+VIEW_WIDTH = 320
+VIEW_HEIGHT = 180
+SCREEN_WIDTH = VIEW_WIDTH * TILE_SIZE
+SCREEN_HEIGHT = VIEW_HEIGHT * TILE_SIZE
 
 SHEET_PATH = "assets/urizen_onebit_tileset__v2d0.png"
 
-SPRITES: dict[str, tuple[int, int, int, int]] = {
+SPRITES_COORDS: dict[str, tuple[int, int, int, int]] = {
     "stone1": (1, 27, 12, 12),
     "stone2": (14, 27, 12, 12),
     "stonecorner1": (66, 27, 12, 12),
@@ -19,19 +24,32 @@ SPRITES: dict[str, tuple[int, int, int, int]] = {
 }
 
 
+def generate_level(width, height, seed=42):
+    random.seed(seed)
+    level = [[1 for _ in range(width)] for _ in range(height)]
+    return level
+
+
 class Game(arcade.Window):
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
         arcade.set_background_color(arcade.color.BLACK)
         self.sheet = arcade.load_spritesheet(SHEET_PATH)
         self.sprites = arcade.SpriteList()
-        self.sprites.append(self.get_sprite("stone1"))
+        self.setup_level()
 
-    def setup(self):
-        pass
+    def setup_level(self):
+        level_int = generate_level(VIEW_WIDTH, VIEW_HEIGHT)
+        for j in range(VIEW_WIDTH):
+            for i in range(VIEW_HEIGHT):
+                if level_int == 1:
+                    wall = self.get_sprite("stone1")
+                    wall.center_x = j * TILE_SIZE + TILE_SIZE / 2
+                    wall.center_y = i * TILE_SIZE + TILE_SIZE / 2
+                    self.sprites.append(wall)
 
     def get_sprite(self, name: str) -> arcade.Texture:
-        x, y, w, h = SPRITES[name]
+        x, y, w, h = SPRITES_COORDS[name]
         tex = self.sheet.get_texture(arcade.LBWH(x, y, w, h))
         return arcade.Sprite(tex, scale=DISPLAY_SCALE)
 
@@ -51,7 +69,6 @@ class Game(arcade.Window):
 
 def main():
     window = Game()
-    window.setup()
     arcade.run()
 
 
